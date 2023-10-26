a8e07d748b5f26dff076a0672410b3ef36ea3e72
chris tutje <chris.tutje@gmail.com>
2023-10-26 18:14:22 -0500

    uploaded EntertainmentCatalog project
    I created a project called EntertainmentCatalog in Eclipse and synced it with the EntertainmentRepository2. It has been declared as a Spring Starter Project. I gave it Lombok, mySQL Driver, Spring Boot DevTools, Spring Data JPA, Spring Web, and Tymeleaf dependencies.

---
+++ EntertainmentCatalog/src/main/java/container/EntertainmentCatalogApplication.java
@@ -0,0 +1,13 @@
+package container;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntertainmentCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntertainmentCatalogApplication.class, args);
+	}
+
+}
