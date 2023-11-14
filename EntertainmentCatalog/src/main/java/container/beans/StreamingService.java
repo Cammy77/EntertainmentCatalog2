2d7516d02979e40a92626a002cd4233f04656441
Cameron Mockobee <cmockobee1@dmacc.edu>
2023-11-13 20:53:20 -0600

    added beans for streaming service and actor

---
+++ EntertainmentCatalog/src/main/java/container/beans/StreamingService.java
@@ -0,0 +1,16 @@
+package container.beans;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class StreamingService {
+	@Id
+	private String name;
+}
