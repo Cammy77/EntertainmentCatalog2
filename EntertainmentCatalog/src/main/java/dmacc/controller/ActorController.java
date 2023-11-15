7e3e530a6d2245cfd0ec8d9f47cd7066abf1c38b
Taylor Layton <taylorglayton@gmail.com>
2023-11-14 21:24:56 -0600

    update to actor controller and templates

--- EntertainmentCatalog/src/main/java/dmacc/controller/ActorController.java
+++ EntertainmentCatalog/src/main/java/dmacc/controller/ActorController.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import dmacc.beans.Actor;
 import dmacc.repository.ActorRepository;
@@ -16,36 +14,35 @@ public class ActorController {
     @Autowired
     private ActorRepository actorRepo;
 
-    @GetMapping("/viewAllActors")
+    @GetMapping({"/viewAllActors"})
     public String viewAllActors(Model model) {
-        model.addAttribute("actors", actorRepo.findAll());
-        return "actorResults";
+        return populateActorsView(model);
     }
-
-    @GetMapping("/inputActor")
+    
+    @GetMapping("/actorInput")
     public String addNewActor(Model model) {
         Actor a = new Actor();
         model.addAttribute("newActor", a);
-        return "inputActor";
+        return "actorInput";
     }
 
-    @PostMapping("/inputActor")
+    @PostMapping("/actorInput")
     public String addNewActor(@ModelAttribute Actor a, Model model) {
         actorRepo.save(a);
-        return "redirect:/viewAllActors";
+        return populateActorsView(model);
     }
 
     @GetMapping("/editActor/{id}")
-    public String editActor(@PathVariable("id") long id, Model model) {
+    public String showUpdateActor(@PathVariable("id") long id, Model model) {
         Actor a = actorRepo.findById(id).orElse(null);
         model.addAttribute("newActor", a);
-        return "inputActor";
+        return "actorInput";
     }
 
     @PostMapping("/updateActor/{id}")
-    public String updateActor(@PathVariable("id") long id, @ModelAttribute Actor a, Model model) {
+    public String reviseActor(@ModelAttribute Actor a, Model model) {
         actorRepo.save(a);
-        return "redirect:/viewAllActors";
+        return populateActorsView(model);
     }
 
     @GetMapping("/deleteActor/{id}")
@@ -54,6 +51,14 @@ public class ActorController {
         if (a != null) {
             actorRepo.delete(a);
         }
-        return "redirect:/viewAllActors";
+        return populateActorsView(model);
+    }
+
+    private String populateActorsView(Model model) {
+        if(actorRepo.findAll().isEmpty()) {
+            return addNewActor(model);
+        }
+        model.addAttribute("actors", actorRepo.findAll());
+        return "actorResults";
     }
 }
